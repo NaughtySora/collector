d3e311a460422a6d5e29f45cb81c05574fc6b809
NaughtySora <naughtysora@proton.me>
2026-09-18 23:38:59 +0300

    cleanup

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 "use strict";
 
 const KeyCollector = require("./lib/KeyCollector.js");
-const Collector = require("./lib/Collector.js");
 
-module.exports = { KeyCollector, Collector };
+module.exports = { KeyCollector };
